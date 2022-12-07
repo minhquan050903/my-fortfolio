c849f54d36c4f7527bb169ef43f2316cc04480c9
Tran Minh Quan Nguyen <90527518+minhquan050903@users.noreply.github.com>
2022-12-07 14:17:33 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+ function otherMess(){
+    var check=document.getElementById('other');
+    var otherMess=getElementById('other-message')
+    if (check.checked == true){
+        otherMess.style.display = "none";
+      }
+}
