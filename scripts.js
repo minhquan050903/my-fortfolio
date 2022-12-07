37bece599b8ae72df74e8db6a0eb365a39a9e6a5
Tran Minh Quan Nguyen <90527518+minhquan050903@users.noreply.github.com>
2022-12-07 16:38:20 -0500

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
- function otherMess(){
-    var check=document.getElementById('other');
-    var otherMess=getElementById('other-message')
-    if (check.checked == true){
-        otherMess.style.display = "none";
-      }
-}
